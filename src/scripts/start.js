19fa8fb61c7d3150800722dc511f1a664acc7c49
Gustavo Roberto <guroberto.dev@gmail.com>
2025-02-28 19:04:27 -0300

    Adição do botão

---
+++ src/scripts/start.js
@@ -0,0 +1,4 @@
+document.getElementById("start-button").addEventListener("click", function () {
+    document.getElementById("start-screen").style.display = "none";
+    document.getElementById("intro").style.display = "flex";
+});

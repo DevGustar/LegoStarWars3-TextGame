14328d0ce280f3f66ac41336afbd16dada226a00
Gustavo Roberto <guroberto.dev@gmail.com>
2025-02-28 20:12:55 -0300

    Adição da música "Main" ao pressionar o botão de start

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -1,4 +1,6 @@
-document.getElementById("start-button").addEventListener("click", function () {
+function tocarMusica() {
     document.getElementById("start-screen").style.display = "none";
+    document.getElementById('MainTitle').volume = 0.3; // Define o volume para 50%
+    document.getElementById('MainTitle').play();
     document.getElementById("intro").style.display = "flex";
-});
+}

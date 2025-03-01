44de2b018adf5ee2c3a8a99b2ea72326d8a8b7f6
Gustavo Roberto <guroberto.dev@gmail.com>
2025-02-28 21:23:12 -0300

    Botão quase finalizado

--- src/scripts/start.js
+++
@@ -1,6 +0,0 @@
-function tocarMusica() {
-    document.getElementById("start-screen").style.display = "none";
-    document.getElementById('MainTitle').volume = 0.3; // Define o volume para 50%
-    document.getElementById('MainTitle').play();
-    document.getElementById("intro").style.display = "flex";
-}
---
+++ src/scripts/utils.js
@@ -0,0 +1,16 @@
+function tocarMusica(idAudio) {
+    document.getElementById("tela-inicial").style.display = "none"; // 
+
+    let musica = document.getElementById(`${idAudio}`);
+    musica.volume = 0.3;
+    musica.play();
+
+    document.getElementById("intro").style.display = "flex";
+
+    // Para a música e muda o display
+    document.querySelector(".crawl").addEventListener("animationend", () => {
+        document.getElementById("intro").style.display = "none";
+        musica.pause();
+        musica.currentTime = 0;
+    });
+}

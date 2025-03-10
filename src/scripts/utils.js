3ce504daa09bcc7816550e9d866029e21d43d535
Gustavo Roberto <guroberto.dev@gmail.com>
2025-03-10 00:10:47 -0300

    Adicionado botão de skip da introdução, pastas e imagens

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,16 +1,27 @@
 function tocarMusica(idAudio) {
-    document.getElementById("tela-inicial").style.display = "none"; // 
+    document.getElementById("tela-inicial").style.display = "none";
 
-    let musica = document.getElementById(`${idAudio}`);
-    musica.volume = 0.1;
+    let musica = document.getElementById(idAudio);
+    musica.volume = 0.05;
     musica.play();
 
+    // Aguarda 15 segundos antes de exibir o botão de pular
+    setTimeout(() => {
+        document.getElementById("skip-button").style.display = "flex";
+    }, 15000);
+    // Exibe a introdução
     document.getElementById("intro").style.display = "flex";
-
-    // Para a música e muda o display
+    // Para a música e muda o display ao fim da animação
     document.querySelector(".crawl").addEventListener("animationend", () => {
-        document.getElementById("intro").style.display = "none";
-        musica.pause();
-        musica.currentTime = 0;
+        pularIntro();
     });
+}
+
+function pularIntro() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("skip-button").style.display = "none";
+
+    let musica = document.getElementById("MainTitle");
+    musica.pause();
+    musica.currentTime = 0;
 }
